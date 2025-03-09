ed61c26c40a6550de8af5453fae7f1af52b4ee10
Lucie monteiro <lucie.mtro@gmail.com>
2025-03-09 20:10:43 +0100

    Add movie components and API routes for top-rated, upcoming, and now playing films

--- app/lib/tmdbAuth.ts
+++ app/lib/tmdbAuth.ts
@@ -18,22 +18,3 @@ export async function redirectToTMDBAuth() {
   const requestToken = await getTMDBRequestToken();
   window.location.href = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${window.location.origin}/tmdb-callback`;
 }
-
-export async function getNowPlayingMovies(language: string) {
-  try {
-    const response = await fetch("/api/tmdb/movies/now-playing", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ language }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Erreur lors de la récupération des films en salle");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Erreur API:", error);
-    return [];
-  }
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import PopularMovies from "./components/movies/popular/PopularMovies";
+import NowPlaying from "./components/movies/now-playing/NowPlayingMovies";
+import TopRatedMovies from "./components/movies/top-rated/TopRatedMovies";
+import UpcomingMovies from "./components/movies/upcoming/UpcomingMovies";
 
 export default function Home() {
   return (
     <main>
       <PopularMovies />
+      <NowPlaying />
+      <TopRatedMovies />
+      <UpcomingMovies />
     </main>
   );
 }
